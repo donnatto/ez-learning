60bc0e6aeefc4a30d31b404f830e274a19052770
Edwin Dominguez <edwindominguez@hotmail.com>
2019-09-28 18:29:06 -0500

    Spring application init

---
+++ src/main/java/com/ezlearning/platform/PlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ezlearning.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformApplication.class, args);
+    }
+
+}
