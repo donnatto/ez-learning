ce2385c4355c9b1c60387a1979967cbce74c9db6
Edwin Dominguez <edwindominguez@hotmail.com>
2019-11-06 23:58:07 -0500

    add courses imgs, modify cursos template, add more cursos in data.sql

--- src/main/resources/db/migration/V1.2__data.sql
+++ src/main/resources/db/migration/V1.2__data.sql
@@ -15,12 +15,26 @@ INSERT INTO profesor (nombre, apellido, correo, descripcion)
 INSERT INTO profesor (nombre, apellido, correo, descripcion)
     VALUES ( 'Pedro', 'Navaja', 'pnavaja@outlook.com', 'Profesor de Python');
 INSERT INTO profesor (nombre, apellido, correo, descripcion)
-    VALUES ( 'Maria', 'Mercedes', 'Mmercedes@yahoo.com', 'Profesora de algoritmos');
+    VALUES ( 'Maria', 'Mercedes', 'Mmercedes@yahoo.com', 'Profesora de algoritmos'),
+           ('Alberto', 'Ibarra', 'aibarra@hotmail.com', 'Profesor de HTML'),
+           ('Jose', 'Domingo', 'jdomingo@hotmail.com', 'Profesor de Linux'),
+           ('Stefany', 'Carranza', 'scarranza@gmail.com', 'Profesora de Javascript'),
+           ('Ismael', 'Lopez', 'slopez@outlook.com', 'Profesor de Base de Datos');
 -- INSERTAR CURSOS
 INSERT INTO curso (nombre, descripcion, profesor_id) VALUES ( 'Essential Java', 'Curso de Java para beginners', 1);
 INSERT INTO curso (nombre, descripcion, profesor_id) VALUES ( 'UX Principles', 'Curso de Experiencia de usuario', 2);
 INSERT INTO curso (nombre, descripcion, profesor_id) VALUES ( 'Advanced Python', 'Curso de Python Avanzado', 3);
 INSERT INTO curso (nombre, descripcion, profesor_id) VALUES ( 'Algorithms 101', 'Curso de Algoritmos para beginners',4);
+INSERT INTO curso (nombre, descripcion, profesor_id) VALUES ( 'HTML Basics', 'Curso de HTML para beginners', 5);
+INSERT INTO curso (nombre, descripcion, profesor_id) VALUES ( 'Advanced HTML', 'Curso de HTML Avanzado', 5);
+INSERT INTO curso (nombre, descripcion, profesor_id) VALUES ( 'Linux Essentials', 'Curso de Linux básico', 6),
+( 'Linux Bash', 'Curso de terminal de Linux', 6),
+( 'JavaScript Essential Training', 'Curso de Javascript Básico',7),
+( 'Advanced JavaScript', 'Curso de Javascript Avanzado',7),
+( 'NodeJS Essential Training', 'Curso de backend con NodeJS',7),
+( 'SQL Essential Training', 'Curso de SQL Básico',8),
+( 'Advanced SQL', 'Curso de SQL Avanzado',8),
+( 'MongoDB Essential Training', 'Curso de MongoDB',8);
 -- INSERTAR MATRICULAS
 -- INSERT INTO matricula (user_id, curso_id, fecha) VALUES ( 1, 3, '2019-10-10 10:30:00' ),
 --                                                         ( 2, 1, '2019-11-01 20:00:00'),
