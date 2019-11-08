486226c8309a61f62433248d2964b9824fe6609f
Edwin Dominguez <edwindominguez@hotmail.com>
2019-11-07 23:39:38 -0500

    add matricula modal, modify scrollbar and footer

--- src/main/resources/static/js/init.js
+++ src/main/resources/static/js/init.js
@@ -5,6 +5,7 @@
     $('.parallax').parallax();
     $('.dropdown-trigger').dropdown({coverTrigger: false, constrainWidth: false});
     $('.collapsible').collapsible();
+    $('.modal').modal();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
